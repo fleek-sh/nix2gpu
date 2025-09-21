321d85e349c2b1d7eec93cb2b22c974b09ae26b8
Luke Bailey <baileylu@tcd.ie>
2025-09-21 18:27:51 +0100

    Reveal secret at runtime as test

--- nix/flake-modules/services.nix
+++ nix/flake-modules/services.nix
@@ -1,24 +1,26 @@
-_:
-{
-  perSystem =
-    _:
-    {
-      process-compose.container-services = {
-        services.nginx."nginx-service" = {
-          enable = true;
-          httpConfig = ''
-            server {
-                listen 8080 default_server;
-                listen [::]:8080 default_server;
-                server_name _;
+_: {
+  perSystem = _: {
+    process-compose.container-services = {
+      services.nginx."nginx-service" = {
+        enable = true;
+        httpConfig = ''
+          server {
+              listen 8080 default_server;
+              listen [::]:8080 default_server;
+              server_name _;
 
-                location / {
-                    add_header Content-Type text/plain;
-                    return 200 "hello world";
-                }
-            }
-          '';
-        };
+              location = /secret {
+                  default_type text/plain;
+                  alias /root/deploy.json;
+              }
+
+              location / {
+                  add_header Content-Type text/plain;
+                  return 200 "hello world";
+              }
+          }
+        '';
       };
     };
+  };
 }
