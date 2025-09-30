0b00273d53a7fe10262e89b67148e2a2b7e34758
Luke Bailey <baileylu@tcd.ie>
2025-09-30 16:48:28 +0100

    Properly define `perContainer` (for real this time)

--- modules/perContainer.nix
+++ modules/perContainer.nix
@@ -1,21 +1,99 @@
-{
-  config,
-  lib,
-  flake-parts-lib,
-  ...
-}:
+{ lib, config, ... }:
 let
-  inherit (lib) types mkMerge;
+  inherit (lib) mkOption types;
+
+  throwShouldBeTopError =
+    param:
+    throw ''
+      `${param}` is not a `perContainer` module argument, but a module argument of
+      the top level config.
+
+      The following is an example usage of `${param}`. Note that its binding
+      is in the `top` parameter list, which is declared by the top level module
+      rather than the `perContainer` module.
+
+        top@{ config, lib, ${param}, ... }: {
+          perContainer = { container, ... }: {
+            # in scope here:
+            #  - ${param}
+            #  - container (of perContainer)
+            #  - top.config (note the `top@` pattern)
+          };
+        }
+    '';
+
+  throwShouldBePerSystemError =
+    param:
+    throw ''
+      `${param}` is not a `perContainer` module argument, but a module argument of
+      `perSystem`.
+
+      The following is an example usage of `${param}`. Note that its binding
+      is in the `perSystem` parameter list, rather than the `perContainer` module.
+
+        top@{ config, lib, , ... }: {
+          perSystem = { ${param}, ... }: {
+            # in scope here:
+            #  - ${param}
+            #  - config (of perSystem)
+            #  - top.config (note the `top@` pattern)
+          };
+        }
+    '';
+
+  mkPerContainerType = module: lib.deferredModuleWith { staticModules = [ module ]; };
+  mkPerContainerOption = module: lib.mkOption { type = mkPerContainerType module; };
 in
 {
-  options.perContainer = flake-parts-lib.mkPerSystemOption {
-    description = ''
-      A function that iterates all nix2vast containers
-      and produces an expression for that.
-    '';
-    type = types.listOf (types.functionTo (types.lazyAttrsOf types.unspecified));
-    apply =
-      perContainerDecls:
-      mkMerge (builtins.map (fn: builtins.mapAttrs fn config.nix2vast) perContainerDecls);
+  options = {
+    perContainer = mkOption {
+      description = ''
+        A function from container to flake-like attributes omitting the `<container>` attribute.
+
+        Modules defined here have access to a `container` struct holding the `name` and
+        `options` fields for every container.
+
+        Behaves similarly to `perSystem` from `flake-parts`.
+      '';
+      type = mkPerContainerType (_: {
+        _file = ./perContainer.nix;
+        config = {
+          _module.args.self = throwShouldBeTopError "self";
+          _module.args.inputs = throwShouldBeTopError "inputs";
+
+          _module.args.config = throwShouldBePerSystemError "config";
+          _module.args.getSystem = throwShouldBePerSystemError "getSystem";
+          _module.args.withSystem = throwShouldBePerSystemError "withSystem";
+          _module.args.moduleWithSystem = throwShouldBePerSystemError "moduleWithSystem";
+        };
+      });
+      apply =
+        modules: container:
+        (lib.evalModules {
+          inherit modules;
+          prefix = [
+            "perContainer"
+            container
+          ];
+          specialArgs = { inherit container; };
+          class = "perContainer";
+        }).config;
+    };
+
+    allContainers = mkOption {
+      type = types.lazyAttrsOf types.unspecified;
+      description = "The container-specific config for each of systems.";
+      internal = true;
+    };
   };
+
+  config =
+    let
+      containersList = lib.mapAttrsToList (name: options: { inherit name options; }) config.nix2vast;
+    in
+    {
+      allContainers = lib.genAttrs containersList config.perContainer;
+
+      flake.lib = { inherit mkPerContainerOption mkPerContainerType; };
+    };
 }
