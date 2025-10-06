489dfdde974519492eafa490e083bb045866db02
Luke Bailey <baileylu@tcd.ie>
2025-10-06 18:59:01 +0100

    Simplify examples

--- examples/basic.nix
+++ examples/basic.nix
@@ -5,5 +5,5 @@
   # This derivation also exposes some scripts,
   # for example, running `nix build .#basic.copyToGithub`
   # will copy it to it's github registry.
-  perSystem = _: { nix2vast.basic = { }; };
+  perSystem.nix2vast.basic = { };
 }
--- examples/services-flake.nix
+++ examples/services-flake.nix
@@ -2,18 +2,16 @@
   # This example shows how one may use
   # [services-flake](https://github.com/juspay/services-flake)
   # config options via the `services` attribute
-  perSystem = _: {
-    nix2vast."with-services-flake" = {
-      services.clickhouse."clickhouse-example" = {
-        enable = true;
-        extraConfig = {
-          http_port = 9050;
-        };
+  perSystem.nix2vast."with-services-flake" = {
+    services.clickhouse."clickhouse-example" = {
+      enable = true;
+      extraConfig = {
+        http_port = 9050;
       };
+    };
 
-      exposedPorts = {
-        "9050/tcp" = { };
-      };
+    exposedPorts = {
+      "9050/tcp" = { };
     };
   };
 }
