434b1fbba9257d9419409525326e92b0aebc4dec
Luke Bailey <baileylu@tcd.ie>
2025-11-03 18:20:26 +0000

    Add ai related substituters from `nixified-ai`

--- flake.nix
+++ flake.nix
@@ -32,16 +32,22 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://ai.cachix.org"
       "https://cache.garnix.io"
       "https://cache.nixos.org"
+      "https://cuda-maintainers.cachix.org"
       "https://huggingface.cachix.org"
       "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "huggingface.cachix.org-1:ynTPbLS0W8ofXd9fDjk1KvoFky9K2jhxe6r4nXAkc/o="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
     ];
   };
 
