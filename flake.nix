054fec39fd50ae3a2c56e0f3866357325b964f39
Luke Bailey <baileylu@tcd.ie>
2025-11-03 18:10:51 +0000

    Add nix community substituter

--- flake.nix
+++ flake.nix
@@ -32,14 +32,16 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://cache.nixos.org"
       "https://cache.garnix.io"
+      "https://cache.nixos.org"
       "https://huggingface.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "huggingface.cachix.org-1:ynTPbLS0W8ofXd9fDjk1KvoFky9K2jhxe6r4nXAkc/o="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
