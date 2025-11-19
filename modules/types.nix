4c2824949b87cead7ec5a7114498b2b580f0ca12
Luke Bailey <baileylu@tcd.ie>
2025-11-19 17:18:56 +0000

    Make internal types internal

--- modules/types.nix
+++ modules/types.nix
@@ -22,26 +22,32 @@ in
     };
     userDef = types.submodule {
       options.uid = mkOption {
+        internal = true;
         type = types.ints.u32;
         description = "User id";
       };
       options.gid = mkOption {
+        internal = true;
         type = types.ints.u32;
         description = "Group id";
       };
       options.shell = mkOption {
+        internal = true;
         type = types.str;
         description = "Login shell path";
       };
       options.home = mkOption {
+        internal = true;
         type = types.str;
         description = "Home directory";
       };
       options.groups = mkOption {
+        internal = true;
         type = types.listOf types.str;
         description = "Supplementary groups";
       };
       options.description = mkOption {
+        internal = true;
         type = types.str;
         description = "User description";
       };
@@ -50,6 +56,7 @@ in
       options.gid = mkOption {
         type = types.ints.u32;
         description = "Group id";
+        internal = true;
       };
     };
   };
