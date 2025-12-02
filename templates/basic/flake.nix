99d58b32aa47a5d9a8830ad6f8a403f659227c09
Luke Bailey <baileylu@tcd.ie>
2025-12-02 17:38:52 +0000

    Add basic downstream flake template

---
+++ templates/basic/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Basic nix2gpu template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    nix2gpu.url = "path:../..";
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      systems,
+      nixpkgs,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ./default.nix ];
+      systems = import systems;
+
+      perSystem =
+        { system, ... }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            config = {
+              cudaSupport = true;
+              allowUnfree = true;
+            };
+          };
+        };
+    };
+}
