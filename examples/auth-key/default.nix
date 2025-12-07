6ee57f7ce61d02215436df1da36a01b4d1514953
Luke Bailey <baileylu@tcd.ie>
2025-12-07 19:00:32 +0000

    Rename agenix example to auth-key

---
+++ examples/auth-key/default.nix
@@ -0,0 +1,18 @@
+{
+  # This example shows how one may use
+  # [agenix](https://github.com/ryantm/agenix)
+  # config options via the `age` attribute
+  perSystem.nix2gpu."auth-key" =
+    { config, ... }:
+    {
+      age.secrets.tailscaleKey = {
+        file = ./_secrets/example.age;
+        path = "/run/secrets/example";
+      };
+
+      tailscale = {
+        enable = true;
+        authKey = config.age.secrets.tailscaleKey.path;
+      };
+    };
+}
