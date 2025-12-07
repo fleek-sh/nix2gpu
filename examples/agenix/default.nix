c8a3f24a3468ce97efbd666a4e291b41c02907a3
Luke Bailey <baileylu@tcd.ie>
2025-12-07 18:12:03 +0000

    Setup proper agenix example

--- examples/agenix/default.nix
+++ examples/agenix/default.nix
@@ -3,8 +3,8 @@
   # [agenix](https://github.com/ryantm/agenix)
   # config options via the `age` attribute
   perSystem.nix2gpu."agenix" = {
-    age.secrets.tailscale-key = {
-      file = ./example.age;
+    age.secrets.hello-world = {
+      file = ./_secrets/example.age;
       path = "/run/secrets/example";
     };
   };
