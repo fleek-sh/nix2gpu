7fcae19cbda1bea44d39d1a436930c505fe55c44
Luke Bailey <baileylu@tcd.ie>
2025-12-21 17:03:16 +0000

    `resholvify` copyToGithub script

--- modules/container/scripts/copy-to-github.nix
+++ modules/container/scripts/copy-to-github.nix
@@ -11,42 +11,51 @@
       perContainer =
         { container, ... }:
         {
-          scripts.copyToGithub = pkgs.writeShellApplication {
-            name = "${container.name}-copy-to-github-registry";
-            runtimeInputs = with pkgs; [
-              gh
-              inputs'.nix2container.packages.skopeo-nix2container
-            ];
-            text = ''
-              if ! gh auth status &>/dev/null; then
-                echo "[nix2gpu] please log in to github first"
-                gh auth login --scopes write:packages
-              fi
+          scripts.copyToGithub =
+            pkgs.resholve.writeScriptBin "${container.name}-copy-to-github-registry"
+              {
+                interpreter = lib.getExe pkgs.bash;
+                inputs = with pkgs; [
+                  gh
+                  inputs'.nix2container.packages.skopeo-nix2container
+                  coreutils
+                ];
+                execer = [
+                  "cannot:${lib.getExe pkgs.gh}"
+                  "cannot:${lib.getExe inputs'.nix2container.packages.skopeo-nix2container}"
+                ];
+              }
+              ''
+                set -euo pipefail
 
-              ${lib.optionalString (container.options.registries == [ ]) ''
-                printf '\n\033[31mError:\033[0m %s.\n' 'In order to use "copyToGithub" the "registry" attribute of your nix2gpu container (${container.name}) must be set' >&2
-                exit 1
-              ''}
+                if ! gh auth status &>/dev/null; then
+                  echo "[nix2gpu] please log in to github first"
+                  gh auth login --scopes write:packages
+                fi
 
-              # shellcheck disable=SC2043,SC2016
-              for registry in ${builtins.concatStringsSep " " container.options.registries}; do
-                IMAGE="${container.name}:${container.options.tag}"
+                ${lib.optionalString (container.options.registries == [ ]) ''
+                  printf '\n\033[31mError:\033[0m %s.\n' 'In order to use "copyToGithub" the "registry" attribute of your nix2gpu container (${container.name}) must be set' >&2
+                  exit 1
+                ''}
 
-                GITHUB_USER="$(gh api user --jq .login)"
-                GITHUB_TOKEN="$(gh auth token)"
+                # shellcheck disable=SC2043,SC2016
+                for registry in ${builtins.concatStringsSep " " container.options.registries}; do
+                  IMAGE="${container.name}:${container.options.tag}"
 
-                echo "[nix2gpu] pushing $IMAGE to $registry..."
-                skopeo copy \
-                  --insecure-policy \
-                  --dest-creds="$GITHUB_USER:$GITHUB_TOKEN" \
-                  nix:"$(readlink -f ${self'.packages.${container.name}})" \
-                  "docker://$registry/$IMAGE"
+                  GITHUB_USER="$(gh api user --jq .login)"
+                  GITHUB_TOKEN="$(gh auth token)"
 
-                echo "[nix2gpu] successfully pushed $registry/$IMAGE"
-                echo "[nix2gpu] pull with: docker pull $registry/$IMAGE"
-              done
-            '';
-          };
+                  echo "[nix2gpu] pushing $IMAGE to $registry..."
+                  skopeo copy \
+                    --insecure-policy \
+                    --dest-creds="$GITHUB_USER:$GITHUB_TOKEN" \
+                    nix:"$(readlink -f ${self'.packages.${container.name}})" \
+                    "docker://$registry/$IMAGE"
+
+                  echo "[nix2gpu] successfully pushed $registry/$IMAGE"
+                  echo "[nix2gpu] pull with: docker pull $registry/$IMAGE"
+                done
+              '';
         };
     };
 }
