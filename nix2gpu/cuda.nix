3b5066f495dc1db7efcc67c3e9ebc1add00f6c41
Luke Bailey <baileylu@tcd.ie>
2026-01-14 14:33:01 +0000

    Properly spec out cuda toggle

--- nix2gpu/cuda.nix
+++ nix2gpu/cuda.nix
@@ -13,61 +13,45 @@ let
     ;
 
   cfg = config.cuda;
-
-  cudaType = types.submodule {
-    options = {
-      enable = mkOption {
-        description = ''
-          If `nix2gpu`'s cuda integration should be enabled or not
-        '';
-        example = literalExpression ''
-          cudaPackages = pkgs.cudaPackages_11_8;
-        '';
-        type = types.bool;
-        default = true;
-      };
-      packages = mkOption {
-        description = ''
-          The set of CUDA packages to be used in the container.
-
-          This option allows you to select a specific version of the CUDA toolkit
-          to be installed in the container. This is crucial for ensuring
-          compatibility with applications and machine learning frameworks that
-          depend on a particular CUDA version.
-
-          The value should be a package set from `pkgs.cudaPackages`. You can find
-          available versions by [searching for `cudaPackages` in Nixpkgs](https://ryantm.github.io/nixpkgs/languages-frameworks/cuda/).
-        '';
-        example = literalExpression ''
-          cuda.packages = pkgs.cudaPackages_11_8;
-        '';
-        type = config.nix2gpuTypes.cudaPackageSet;
-        default = pkgs.cudaPackages_13_0;
-        defaultText = literalExpression "pkgs.cudaPackages_13_0";
-      };
-    };
-  };
 in
 {
   _class = "nix2gpu";
 
   options.cuda = {
-    description = ''
-      Cuda configuration options for `nix2gpu`.
+    enable = mkOption {
+      description = ''
+        If `nix2gpu`'s cuda integration should be enabled or not
+      '';
+      example = literalExpression ''
+        cudaPackages = pkgs.cudaPackages_11_8;
+      '';
+      type = types.bool;
+      default = true;
+    };
+    packages = mkOption {
+      description = ''
+        The set of CUDA packages to be used in the container.
 
-      Allows you to configure the behaviour of `cuda` in a `nix2gpu` context.
-    '';
-    example = literalExpression ''
-      cuda.enable = true;
-      cuda.packages = pkgs.cudaPackages_11_8;
-    '';
-    type = cudaType;
-    default = { };
+        This option allows you to select a specific version of the CUDA toolkit
+        to be installed in the container. This is crucial for ensuring
+        compatibility with applications and machine learning frameworks that
+        depend on a particular CUDA version.
+
+        The value should be a package set from `pkgs.cudaPackages`. You can find
+        available versions by [searching for `cudaPackages` in Nixpkgs](https://ryantm.github.io/nixpkgs/languages-frameworks/cuda/).
+      '';
+      example = literalExpression ''
+        cuda.packages = pkgs.cudaPackages_11_8;
+      '';
+      type = config.nix2gpuTypes.cudaPackageSet;
+      default = pkgs.cudaPackages_13_0;
+      defaultText = literalExpression "pkgs.cudaPackages_13_0";
+    };
   };
 
   config = mkIf cfg.enable {
     # TODO[b7r6]: Pick the right ones
-    systemPackages = with config.cudaPackages; [
+    systemPackages = with cfg.packages; [
       cudatoolkit
       cudnn
       # cusparselt
@@ -77,5 +61,18 @@ in
       nccl
       pkgs.nvtopPackages.nvidia
     ];
+
+    extraEnv = {
+      CUDA_VERSION = cfg.packages.cudaMajorMinorVersion;
+      NVIDIA_DISABLE_REQUIRE = "0";
+      NVIDIA_DRIVER_CAPABILITIES = "compute,utility,graphics";
+      NVIDIA_REQUIRE_CUDA = "cuda>=11.0";
+      NVIDIA_VISIBLE_DEVICES = "all";
+    };
+
+    extraLabels = {
+      "com.nvidia.volumes.needed" = "nvidia_driver";
+      "com.nvidia.cuda.version" = cfg.packages.cudatoolkit.version;
+    };
   };
 }
