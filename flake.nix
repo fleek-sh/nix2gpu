1189af3677bc0f9af770d22f66d3e8aff4960b2b
Luke Bailey <baileylu@tcd.ie>
2026-01-29 15:04:08 +0000

    `nix flake update` + switch nimi feature branch

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     };
 
     nimi = {
-      url = "github:weyl-ai/nimi/baileylu/minimize-flake";
+      url = "github:weyl-ai/nimi/baileylu/bubblewrap";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
