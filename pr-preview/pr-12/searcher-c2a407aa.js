d193a147d812a6a021de7ec8c8a02beb6521adbe
baileylu121 <baileylu121@users.noreply.github.com>
2026-02-01 17:42:32 +0000

    Deploy preview for PR 12 🛫

--- pr-preview/pr-12/searcher-c2a407aa.js
+++ pr-preview/pr-12/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-074c4f61.js',
+                path_to_root + 'searchindex-d32af137.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
